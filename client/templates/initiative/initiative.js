3791f2fd5f50e96ed885b2b34dedebafd06d5ca8
Lasse Larsen <lasse@bisontiger.com>
2015-06-09 14:31:33 -0700

    Revert "Update layout with scss"
    This reverts commit 9172ebb1becb7b2a320454be4282b47355f3f3c0.

--- client/templates/initiative/initiative.js
+++ client/templates/initiative/initiative.js
@@ -1,8 +1,5 @@
 Template.initiative.helpers({
 	image: function(){
 		return "https://placeimg.com/300/250/arch";
-	},
-	userImage: function(){
-		return "https://placeimg.com/60/60/people";
 	}
 });
