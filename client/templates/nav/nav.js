3a62d901a556d8e104e32adb0fa13d2a37cc879a
Lasse Larsen <lasse@bisontiger.com>
2015-06-09 16:29:56 -0700

    Add login route

--- client/templates/nav/nav.js
+++ client/templates/nav/nav.js
@@ -1,3 +1,12 @@
 Template.nav.onRendered(function() {
   this.$('.button-collapse').sideNav();
 });
+
+Template.nav.events({
+	'click .logout': function(e, tpl){
+		e.preventDefault();
+		Meteor.logout(function(){
+			sAlert.info('Logged out succesfully');
+		});
+	}
+})
