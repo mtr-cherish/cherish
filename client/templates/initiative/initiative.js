9bab71351269efbe1f4e0dbc01f3892f52e53da3
Lasse Larsen <lasse@bisontiger.com>
2015-06-09 19:08:28 -0700

    Add vote action for user, and alert for public

--- client/templates/initiative/initiative.js
+++ client/templates/initiative/initiative.js
@@ -6,3 +6,15 @@ Template.initiative.helpers({
     return "https://placeimg.com/60/60/people";
   }
 });
+
+Template.initiative.events({
+  'click .votes': function(e, tpl){
+    // TODO: restrict votes to 1 pr initiative
+    if(Meteor.userId()){
+      Initiatives.update(this._id, {$inc: {votes: 1}});
+      sAlert.info('Thanks for voting on: '+ this.title);
+    } else {
+      sAlert.error('Can only vote as logged in user');
+    }    
+  }
+});
