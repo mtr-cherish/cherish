43f5d81004ec33881daecf2de27a19a97f0fa467
mariocatch <mariocatch@gmail.com>
2015-06-09 20:30:36 -0400

    Code cleanup

--- client/templates/initiative/initiative.js
+++ client/templates/initiative/initiative.js
@@ -1,8 +1,8 @@
 Template.initiative.helpers({
-	image: function(){
-		return "https://placeimg.com/300/250/arch";
-	},
-	userImage: function(){
-		return "https://placeimg.com/60/60/people";
-	}
+  image: function(){
+    return "https://placeimg.com/300/250/arch";
+  },
+  userImage: function(){
+    return "https://placeimg.com/60/60/people";
+  }
 });
--- client/templates/nav/nav.js
+++ client/templates/nav/nav.js
@@ -1,12 +1,14 @@
 Template.nav.onRendered(function() {
-  this.$('.button-collapse').sideNav();
+  this.$('.button-collapse').sideNav({
+    closeOnClick: true
+  });
 });
 
 Template.nav.events({
-	'click .logout': function(e, tpl){
-		e.preventDefault();
-		Meteor.logout(function(){
-			sAlert.info('Logged out succesfully');
-		});
-	}
+  'click .logout': function(e, tpl){
+    e.preventDefault();
+    Meteor.logout(function(){
+      sAlert.info('Logged out succesfully');
+    });
+  }
 })
