9f1e7fe1f75d655d7ecc84070dff000edb7ce1fc
iamlasse <justlasse@gmail.com>
2015-06-14 22:58:51 -0700

    Amend filter search fix, still kinda wonky

--- client/templates/initiatives/initiatives.js
+++ client/templates/initiatives/initiatives.js
@@ -1,14 +1,14 @@
 Template.initiatives.helpers({
   initiatives: function(){
     var cursor = {};
-    if (Session.get('categories') && !Session.get('searchTerm')) {
-      cursor = _.extend(Initiatives.find({categorySlug: {$in: Session.get('categories')}}, {sort: {votes: -1}}), cursor);
-    }
-    else if(Session.get('searchTerm')) {
-      cursor = _.extend(InitiativeSearch.getData({categorySlug: {$in: Session.get('categories')}}, {
+    if(Session.get('searchTerm')) {
+      cursor = _.extend(InitiativeSearch.getData({}, {
         sort: {votes: -1}
       }), cursor);
-    } else {
+    }
+    if (Session.get('categories')) {
+      cursor = _.extend(Initiatives.find({categorySlug: {$in: Session.get('categories')}}, {sort: {votes: -1}}), cursor);
+    } else if(!Session.get('categories') || !Session.get('searchTerm')) {
       cursor = _.extend(Initiatives.find({}, {sort: {votes: -1, title: -1}}), cursor);
     }
     return cursor;
