74c7c389eb59061ed1879b4e17a4ecc9c96b8084
mariocatch <mariocatch@gmail.com>
2015-06-15 23:23:35 -0400

    Add creation form, error checking with sAlerts, unique category selection, session storage and sAlert creation confirmation. TODO: Insert into Initiatives db.

--- client/templates/initiative/create/initiativeCreate.js
+++ client/templates/initiative/create/initiativeCreate.js
@@ -1,4 +1,43 @@
 Template.initiativeCreate.onRendered(function() {
   $('select').material_select();
-  $('input[name="name"]').focus();
+});
+
+Template.initiativeCreate.events({
+  'submit .form-create': function (event, template) {
+    event.preventDefault();
+
+    var title = template.find('input[name=name]').value || undefined;
+    var description = template.find('input[name=description]').value || undefined;
+    var category;
+    $('input[type=checkbox]:checked').each(function() {
+      category = $(this).val() || undefined;
+    });
+
+    if(!title) {
+      sAlert.error('You must enter a name for your Initiative.');
+    }
+    if(!description) {
+      sAlert.error('You must enter a description for your Initiative.');
+    }
+    if(!category) {
+      sAlert.error('You must select a category for your Initiative.');
+    }
+
+    if(!title || !description || !category) {
+      return;
+    }
+
+    Session.set('initiativeCreated', true);
+    Router.go('initiatives');
+  },
+
+  'click input[type=checkbox]': function(event, template) {
+    $('.checkboxes').find('input[type=checkbox]:checked').each(function() {
+      if($(this).val() == event.target.value) {
+        $(this).attr('checked', true);
+      } else {
+        $(this).attr('checked', false);
+      }
+    });
+  }
 });
