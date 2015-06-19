0d4d391e6cbcf83dd13763405826d52402b6ce9d
jeffshaver <jeff.e.shaver@gmail.com>
2015-06-18 20:07:14 -0400

    Inital attempt to move some template helpers into collection helpers

--- client/templates/initiative/initiativeHelpers.js
+++ client/templates/initiative/initiativeHelpers.js
@@ -1,32 +1,9 @@
-Template.registerHelper('getInitiativeImage', function(context) {
-  return this.imageUrl
-});
-
-Template.registerHelper('getInitiativeAuthorImage', function(context) {
-  return "https://placeimg.com/60/60/people";
-});
-
-Template.registerHelper('getInitiativeCategory', function(context) {
-  return this.category.toLowerCase();
-});
-
-Template.registerHelper('getInitiativeCategoryClass', function(context) {
+Template.registerHelper('getInitiativeCategoryClass', function() {
   return s.slugify(this.category);
 });
 
 Template.registerHelper('getPrettyDate', function(timestamp) {
   return moment(new Date(timestamp)).fromNow();
-})
-
-Template.registerHelper('canVote', function(context){
-  // return true if initiative Id is not in votedOn array
-  var user = Meteor.user();
-  if(user){
-    if(_.contains(user.votedOn, this._id)){
-      return false;
-    }
-  }
-  return true;
 });
 
 Template.registerHelper('zeroIfEmptyOrNotExists', function(context) {
@@ -44,17 +21,17 @@ addOrRemoveVote = function(initiative){
   if(user){
     if(_.contains(user.votedOn, initiative._id)){
       Initiatives.update(initiative._id, {
-        $inc: { votes: -1 }, 
+        $inc: { votes: -1 },
         $pull: { usersVoted: user._id }
       });
       Meteor.users.update(Meteor.userId(), {$pull: {"votedOn": initiative._id}});
       sAlert.error('Ok, We removed your vote...');
     } else {
       Initiatives.update(initiative._id, {
-        $inc: { votes: 1 }, 
+        $inc: { votes: 1 },
         $addToSet: { usersVoted: user._id }
       });
-      Meteor.users.update(Meteor.userId(), {$addToSet: {"votedOn": initiative._id}});  
+      Meteor.users.update(Meteor.userId(), {$addToSet: {"votedOn": initiative._id}});
       sAlert.info('Thanks for voting on: '+ initiative.title);
     }
   } else {
