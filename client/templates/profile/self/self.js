9a97babec35a8d9ab41843c63103842e09871a1c
iamlasse <justlasse@gmail.com>
2015-06-20 13:08:08 -0700

    Move helpers to collection helper, Users.initiativeCount, Users.followerCount, Users.votedOnCount

--- client/templates/profile/self/self.js
+++ client/templates/profile/self/self.js
@@ -8,16 +8,5 @@ Template.profileSelf.helpers({
     return Initiatives.find({
       'comments.createdBy': Meteor.userId()
     });
-  },
-  votedOnCount: function(){
-    var votedOnCount = Initiatives.find({usersVoted: Meteor.userId()}).count();
-    return votedOnCount;
-  },
-  initiativesCount: function() {
-    var initiativesCount = Initiatives.find({createdBy: Meteor.userId(), active: true}).count();
-    return initiativesCount;
-  },
-  followerCount: function(){
-    return 1;
   }
 })
