e1bb2a6b9c54bc14ccf495c35a10c3a35e42d190
iamlasse <justlasse@gmail.com>
2015-06-22 21:08:34 -0700

    Begin work on cron jobs

---
+++ server/cron/notifications.js
@@ -0,0 +1,23 @@
+SyncedCron.add({
+  name: 'Remove older notifications',
+  schedule: function(parser) {
+    // parser is a later.parse object
+    return parser.text('every 2 hours');
+  },
+  job: function() {
+    // Remove notifications older than x days
+    var d          = new Date(),
+        oneWeekOld = d.setMinutes(d.getMinutes() - 10080),
+        notifications = Meteor.users.find({"notifications.createdAt": { $lt: oneWeekOld }}).fetch();
+
+    _.each(notifications, function(value, key, list){
+      console.log('value: ', value);
+      console.log('key: ', key);
+      // Meteor.users.update({"notifications.createdAt": { $lt: oneWeekOld } }, {"$pullAll": {
+      //   "notifications.$.createdAt":  
+      // }})
+    })    
+    
+
+  }
+});
