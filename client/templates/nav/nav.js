752cd9b4dcbad4b34474ec00d69a5cd76a78edc0
jeffshaver <jeff.e.shaver@gmail.com>
2015-06-23 21:51:34 -0400

    Intial attempt at rollup notifications

--- client/templates/nav/nav.js
+++ client/templates/nav/nav.js
@@ -27,7 +27,7 @@ Template.userDropdown.helpers({
 });
 
 Template.notificationsDropdown.onRendered(function() {
-  var instance = Template.instance(); 
+  var instance = Template.instance();
 
   Tracker.autorun(function () {
     if(instance.view.template.__helpers[" hasNotifications"]()){
@@ -44,8 +44,14 @@ Template.notificationsDropdown.helpers({
     return Notifications.find({ownerId: Meteor.userId(), isRead: false});
   },
   notifications: function() {
-    if(Meteor.user())
-      return Notifications.find({ownerId: Meteor.userId()}, {sort: {createdAt: -1}});
+    var notifications;
+    if (!Meteor.user()) {
+      return undefined;
+    }
+    notifications = Notifications.find({ownerId: Meteor.userId()}, {sort: {createdAt: -1}});
+
+    return rollUpNotifications(notifications);
+
   },
   getNotificationsCount: function() {
     if(Meteor.user())
@@ -82,7 +88,7 @@ Template.notificationsDropdown.helpers({
       return " stopped following ";
       break;
     }
-    
+
   },
   getReadClass: function() {
     return this.isRead ? 'read' : 'unread';
@@ -93,12 +99,14 @@ Template.notificationsDropdown.helpers({
   getInitiativeSlug: function(){
     return Initiatives.findOne({_id: this.initiativeId}).slug;
   },
-  getUserWhoTriggeredNotification: function(userId){
-    var user = Meteor.users.findOne({_id: userId});
-    if(user.profile.name)
-      return user.profile.name;
-
-    return user.username;
+  getUsersWhoTriggeredNotification: function(userIds){
+    var users = Meteor.users.find({_id: {$in: userIds}});
+    return users.fetch().map(function(user) {
+      if (user.profile.name) {
+        return user.profile.name;
+      }
+      return user.username;
+    });
   }
 });
 
@@ -119,4 +127,74 @@ Template.notificationsDropdown.events({
 
 Template.registerHelper('getAvatar', function(userId){
   return Meteor.users.findOne({_id: userId}).profile.avatarImg;
-})
+});
+
+function getUserNameById(userId){
+  var user = Meteor.users.findOne({_id: userId});
+  return user.profile.name ? user.profile.name : user.username;
+}
+
+function edAWord(word) {
+  var suffix = 'ed';
+  if (word[word.length - 1] === 'e') {
+    suffix = 'd';
+  }
+  return word + suffix;
+}
+
+function rollUpNotifications(notifications) {
+  var rolledUpNotifications = [];
+  var unreadNotifications = [];
+  var notificationDates = {};
+  var notificationsObj = {};
+  var userArray, users, othersText, lastUser, initiativeId, type;
+
+  notifications.forEach(function(notification) {
+    var initiativeId = notification.initiativeId;
+    var type = notification.type;
+    if (!notificationsObj[initiativeId]) {
+      notificationsObj[initiativeId] = {};
+    }
+    if (!notificationsObj[initiativeId][type]) {
+      notificationsObj[initiativeId][type] = [];
+    }
+    if (!notificationDates[type + initiativeId] || notification.createdAt > notificationDates[type + initiativeId]) {
+      notificationDates[type + initiativeId] = notification.createdAt;
+    }
+    if (notificationsObj[initiativeId][type].indexOf(notification.userId)) {
+      notificationsObj[initiativeId][type].push(notification.userId);
+    }
+    if (notification.isRead || unreadNotifications.indexOf(type + initiativeId) !== -1) {
+      return;
+    }
+    unreadNotifications.push(type + initiativeId);
+  });
+
+  console.log(notificationsObj);
+
+  for (initiativeId in notificationsObj) {
+    for (type in notificationsObj[initiativeId]) {
+      userArray = notificationsObj[initiativeId][type];
+      users = userArray.slice(0, 3).map(function(userId) {
+        return getUserNameById(userId);
+      });
+      userText = users.join(', ') + ' ';
+      lastUser = othersText = '';
+      if (userArray.length > 3) {
+        othersText = 'and ' + userArray.slice(3).length + ' others ';
+      } else if (userArray.length <= 3 && userArray.length > 1) {
+        lastUser = users.pop();
+        userText = users.join(', ') + ' and ' + lastUser + ' ';
+      }
+      rolledUpNotifications.push({
+        users: userText,
+        text: edAWord(type) + ' on',
+        othersText: othersText,
+        initiativeId: initiativeId,
+        isRead: unreadNotifications.indexOf(type + initiativeId) === -1,
+        createdAt: notificationDates[type + initiativeId]
+      });
+    }
+  }
+  return rolledUpNotifications;
+}
