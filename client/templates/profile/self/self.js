40ecdd7c2e8688ed34cccbfbbcbc1a7d1b5e8afd
Dylan Sanders <sandersd27@gmail.com>
2015-06-26 19:58:15 -0700

    Added file upload for profile page

--- client/templates/profile/self/self.js
+++ client/templates/profile/self/self.js
@@ -8,5 +8,33 @@ Template.profileSelf.helpers({
     return Initiatives.find({
       'comments.createdBy': Meteor.userId()
     });
+  },
+  editingPicture: function(){
+    return Session.get('editingPicture');
   }
-})
+});
+
+Template.profileSelf.events({
+  'change .fileInput': function (event, template) {
+    FS.Utility.eachFile(event, function(file) {
+      Images.insert(file, function(err, fileObj) {
+        if(err) {
+          // Handle Error
+          console.log(err);
+        } else{
+          userId = Meteor.userId();
+          imagesURL = {
+          "profile.avatarImg": "cfs/files/images/" + fileObj._id
+          };
+        }
+      });
+    });
+  },
+  'click #upload': function () {
+    Meteor.users.update(userId, {$set: imagesURL});
+    Session.set('editingPicture', null);
+  },
+  'click #editPicture': function () {
+    Session.set('editingPicture', true);
+  }
+});
