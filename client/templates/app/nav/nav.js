fd0a0c658acbba63c322cb6167223833beaf9693
iamlasse <justlasse@gmail.com>
2015-06-28 23:50:56 -0700

    Fix nav to only use menu icon on small screen and logo is a link now

--- client/templates/app/nav/nav.js
+++ client/templates/app/nav/nav.js
@@ -24,3 +24,15 @@ Template.userDropdown.helpers({
     return Meteor.user().profile.avatarImg;
   }
 });
+
+
+Template.sidenav.onRendered(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    this.$('.open-notifications').leanModal();
+});
+
+Template.sidenav.events({
+  'click .open-notifications': function(){
+
+  }
+});
