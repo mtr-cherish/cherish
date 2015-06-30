c0ba2cc6790346a133c2b4b7e5cad0b77f048106
iamlasse <justlasse@gmail.com>
2015-06-30 14:42:33 -0700

    New mock data, only one initiative per user Random amount of comment (0 - 10)

--- server/mock.js
+++ server/mock.js
@@ -2,119 +2,82 @@ if(Meteor.settings.mode === 'dev') {
   console.log("======> Staging mock data...");
   var placeholderInitiativeHeaderImage = '/images/placeholder-initiative.jpg';
   var placeholderAvatarImage = "/images/placeholder-avatar.jpg";
+  var users = [];
+  var initiatives = [];
 
-  Meteor.startup(function() {
-    var users;
 
-    if(Meteor.users.find().count() === 0) {
-      console.log('Add test users');
-      var userId = Accounts.createUser({
-        username: 'cherishuser',
-        password: 'password',
-        email: 'admin@cherish.com',
-        profile: {
-          name: 'Test User',
-          avatarImg: placeholderAvatarImage
-        },
-        votedOn: [],
-        commentedOn: []
-      });
-
-      var userId2 = Accounts.createUser({
-        username: 'cherishuser2',
-        password: 'password',
-        email: 'admin2@cherish.com',
-        profile: {
-          name: 'Test User 2',
-          avatarImg: placeholderAvatarImage
-        },
-        votedOn: [],
-        commentedOn: []
-      });
-      console.log(userId);
-      users = [userId, userId2];
-    }
-
-    
-
-    
-
-    if(Initiatives.find().count() === 0) {
-      console.log("Inserting Initiatives");
-      
-      var mockData = [
-        {
-          title: 'Project water', 
-          description: 'Some arbitrary description goes here.', 
-          votes: 20, 
-          createdBy: userId, 
-          category: 'Charity',
-          imageUrl: placeholderInitiativeHeaderImage,
-          active: true,
-        },
-        {title: 'Digging in dirt', description: 'Some arbitrary description goes here.', votes: 5, createdBy: userId, category: 'Event', imageUrl: placeholderInitiativeHeaderImage, active: true},
-        {
-          title: 'Free the pigs', 
-          description: 'Some arbitrary description goes here.', 
-          votes: 100, 
-          createdBy: userId2,
-          category: 'Event',
-          imageUrl: placeholderInitiativeHeaderImage,
-          active: true,
-          comments: []
-        },
-        {title: 'Vegan community', description: 'Some arbitrary description goes here.', votes: 10, createdBy: userId2, category: 'Non Profit', imageUrl: placeholderInitiativeHeaderImage, active: true},
-        {title: 'Spiritual commerce', description: 'Some arbitrary description goes here.', votes: 150, createdBy: userId, category: 'Open Source',imageUrl: placeholderInitiativeHeaderImage, active: true},
-        {title: 'Earth Documentary', description: 'Some arbitrary description goes here.', votes: 20, createdBy: userId, category: 'Charity', imageUrl: placeholderInitiativeHeaderImage, active: true},
-        {
-          title: 'Feature film', 
-          description: 'Some arbitrary description goes here.', 
-          votes: 5, 
-          createdBy: userId,
-          category: 'Event',
-          imageUrl: placeholderInitiativeHeaderImage,
-          active: true,
-          comments: []
-        },
-        {title: 'We\'re going to Japan', description: 'Some arbitrary description goes here.', votes: 100, createdBy: userId, category: 'Charity', imageUrl: placeholderInitiativeHeaderImage, active: true},
-        {title: 'Initiative 9', description: 'Some arbitrary description goes here.', votes: 10, createdBy: userId2, category: 'Event', imageUrl: placeholderInitiativeHeaderImage, active: true},
-        {
-          title: 'Feeding Nicaragua', 
-          description: 'Some arbitrary description goes here.', 
-          votes: 1, 
-          createdBy: userId2,
-          category: 'Charity',
-          imageUrl: placeholderInitiativeHeaderImage,
-          active: true,
-          comments: []
-        }
-      ];
-
-      mockData.forEach(function(item) {
-        console.log("Inserting " + item.title);
-        var initiative = Initiatives.insert(item);
-        var count = _.random(0, 10);
-        
-        
-
-        for(var i = 0; i <= count; i++){
-          console.log('Adding comment');
-          var user = _.random(0, users.length-1);
-          Initiatives.direct.update(initiative, {$addToSet: {comments: {
-              message: "This is a comment.",
-              createdBy: users[user],
-              createdAt: new Date().getTime()
-            }}});
-          var UpdateInitiative = Initiatives.findOne({_id: initiative});
-          if(UpdateInitiative.createdBy !== users[user]){
-            Initiatives.direct.update(UpdateInitiative._id, {$inc: {votes: 1}, $addToSet: {usersVoted: users[user]}});
-            // Remove once votes only stored on Initiative
-            Meteor.users.update(users[user], {$addToSet: {votedOn: UpdateInitiative._id}});
-          }
-        }
-
-     });
-      console.log("======> Mock data staged.");
+  _.mixin({
+    capitalize: function(string) {
+      return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
     }
   });
+
+  
+
+
+  Meteor.startup(function() {
+
+    var getWord = function(){ 
+      var word = faker.lorem.words(1); 
+      return _(word[0]).capitalize();
+    };
+
+    if(Meteor.users.find().count() === 0) {
+      _.each(_.range(25), function(number){
+
+        var randomEmail = faker.internet.email(),
+        randomName = faker.name.findName(),
+        userName = 'cherishuser'+ number,
+        password = 'password';
+        
+
+        var userId = Accounts.createUser({
+          username: userName,
+          profile: {
+            name: randomName,
+          },
+          email: 'cherishuser'+number +'@cherish.com',
+          password: password
+        });  
+
+        users.push(userId);
+
+        var initiative = {
+          title: getWord() + ' ' + faker.lorem.sentence(5),
+          description:  getWord() + ' ' + faker.lorem.paragraph(),
+          votes: faker.random.number(200),
+          category: faker.random.array_element(InitiativeCategories),
+          imageUrl: placeholderInitiativeHeaderImage,
+          createdBy: userId,
+          active: true
+        }
+
+        var initiativeId = Initiatives.insert(initiative);
+
+        // Add to initiative list
+        initiatives.push(initiativeId);
+        
+      });
+
+
+  _.each(initiatives, function(initiative){
+    commentsNumber = faker.random.number(10);
+    _(commentsNumber).times(function(){
+      var user = faker.random.array_element(users);
+      Initiatives.direct.update(initiative, {
+        $addToSet: {
+          usersVoted: user,
+          comments: {
+            message: getWord() + ' ' + faker.lorem.sentences(1),
+            createdBy: user,
+            createdAt: new Date().getTime()
+          }
+        }
+      });
+
+      Users.update(user, { $addToSet: { commentedOn: initiative, votedOn: initiative } });
+    });
+  });
+}
+});
 }
