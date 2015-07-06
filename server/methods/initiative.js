da8ffd05261471afe6a3b856d67219be22523a4a
iamlasse <justlasse@gmail.com>
2015-07-05 21:16:28 -0700

    Update to match Eslint / Es6

--- server/methods/initiative.js
+++ server/methods/initiative.js
@@ -29,59 +29,64 @@ Meteor.methods({
     return true;
   },
 
-  addOrRemoveVote: function(initiative) {
+  addOrRemoveVote: function addOrRemoveVote(initiative) {
     var user = Meteor.user();
 
-    if (!user)
-      throw new Meteor.Error(401, 'Can only vote as logged in user')
+    if (!user) {
+      throw new Meteor.Error(401, 'Can only vote as logged in user');
+    }
 
-    if(user._id === initiative.createdBy)
-      throw new Meteor.Error(401, 'You can\'t vote on your own Initiatives')
+    if (user._id === initiative.createdBy) {
+      throw new Meteor.Error(401, 'You can\'t vote on your own Initiatives');
+    }
 
-    if (!Throttle.checkThenSet('vote', 1, 3000))
+    if (!Throttle.checkThenSet('vote', 10, 1000)) {
       throw new Meteor.Error(500, 'You can only vote every 3 seconds');
+    }
 
     if (_.contains(user.votedOn, initiative._id)) {
       Initiatives.update(initiative._id, {
-        $inc: { votes: -1 },
-        $pull: { usersVoted: user._id }
+        $inc: {votes: -1},
+        $pull: {usersVoted: user._id}
       });
-      Meteor.users.update(Meteor.userId(), {$pull: {'votedOn': initiative._id}});
+      Meteor.users.update(Meteor.userId(), {$pull: {votedOn: initiative._id}});
       return false;
     }
     Initiatives.update(initiative._id, {
-      $inc: { votes: 1 },
-      $addToSet: { usersVoted: user._id }
+      $inc: {votes: 1},
+      $addToSet: {usersVoted: user._id}
     });
-    Meteor.users.update(Meteor.userId(), {$addToSet: {'votedOn': initiative._id}});
+    Meteor.users.update(Meteor.userId(), {$addToSet: {votedOn: initiative._id}});
     return true;
   },
 
-  setInactiveActive: function(initiative){
-    if(initiative.createdBy === Meteor.userId()){
-      var updated = Initiatives.update(initiative._id, {$set: { active: !initiative.active}});
+  setInactiveActive: function setInactiveActive(initiative) {
+    var updated;
+
+    if (initiative.createdBy === Meteor.userId()) {
+      updated = Initiatives.update(initiative._id, {$set: {active: !initiative.active}});
     }
 
-    if(updated){
-      return true;
-    } else {
+    if (!updated) {
       throw new Meteor.Error(500, 'Initiative could not be updated at current');
     }
+    return true;
   },
 
-  addComment: function(initiativeId, input) {
-    check(initiativeId, String);
-    check(input, String);
+  addComment: function addComment(initiativeId, input) {
     var userId = Meteor.userId();
 
+    check(initiativeId, String);
+    check(input, String);
+
     if (!userId) {
-      throw new Meteor.Error(401, 'Can only comment as logged in user')
+      throw new Meteor.Error(401, 'Can only comment as logged in user');
     }
 
-    if (!Throttle.checkThenSet('addcomment', 1, 3000)) {
+    if (!Throttle.checkThenSet('addcomment', 5, 30000)) {
       throw new Meteor.Error(500, 'You can only comment every 3 seconds');
     }
-    Meteor.users.update(userId, { $addToSet: { commentedOn: initiativeId } });
+    Meteor.users.update(userId, {$addToSet: {commentedOn: initiativeId}});
     return Initiatives.update(initiativeId, {$addToSet: {comments: {
       createdBy: userId,
       message: input,
@@ -89,26 +94,35 @@ Meteor.methods({
     }}});
   },
 
-  createInitiative: function(title, summary, category) {
-      var slug = s.slugify(title);
-      var categorySlug = s.slugify(category);
-      var initiative =
-      {
-        title: title,
-        summary: summary,
-        votes: 0,
-        createdBy: Meteor.userId(),
-        createdAt: new Date(),
-        category: category,
-        imageUrl: '/images/placeholder-initiative.jpg',
-        comments: [],
-        slug: slug,
-        categorySlug: categorySlug,
-        active: true
-      }
+  createInitiative: function createInitiative(title, summary, category) {
+    var initiativeLimit = Meteor.settings.public.initiativeLimit;
+    var existingInitiatives = Initiatives.find({createdBy: Meteor.userId(), active: true});
+    var slug;
+    var categorySlug;
+    var initiative;
 
-      Initiatives.insert(initiative);
-      return slug;
+    if (!existingInitiatives && existingInitiatives.count() > initiativeLimit) {
+      throw new Meteor.Error(403, 'You have reached the limit of ' + initiativeLimit + ' initiatives.');
+    }
+
+    slug = s.slugify(title);
+    categorySlug = s.slugify(category);
+    initiative = {
+      title: title,
+      summary: summary,
+      votes: 0,
+      createdBy: Meteor.userId(),
+      createdAt: new Date(),
+      category: category,
+      imageUrl: '/images/placeholder-initiative.jpg',
+      comments: [],
+      slug: slug,
+      categorySlug: categorySlug,
+      active: true
+    };
+
+    Initiatives.insert(initiative);
+    return slug;
   },
 
   updateInitiative: function(initiative, props){
