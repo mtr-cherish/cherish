53394d95f1dea42ee827ad7790353dd4ff439b63
mariocatch <mariocatch@gmail.com>
2015-07-10 00:05:47 -0400

    Clean up conditions for sending email, add server logging

--- server/email.js
+++ server/email.js
@@ -1,9 +1,13 @@
 Meteor.startup(function meteorStartup() {
-  if ( Meteor.settings.enableEmail && Meteor.settings && Meteor.settings.mandrill ) {
+  if ( Meteor.settings && Meteor.settings.enableEmail && Meteor.settings.mandrill ) {
     console.log('======> Email enabled.')
     Meteor.Mandrill.config({
       username: Meteor.settings.mandrill.username,
       key: Meteor.settings.mandrill.key
     });
+
+    return;
   }
+
+  console.log('======> Email disabled.');
 });
