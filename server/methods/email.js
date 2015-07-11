f3a460aefffbed7d8786ce9b404a75003829bdeb
mariocatch <mariocatch@gmail.com>
2015-07-11 00:30:41 -0400

    [eslint]: ignore mandrill naming conventions

--- server/methods/email.js
+++ server/methods/email.js
@@ -1,11 +1,13 @@
 Meteor.methods({
   sendEmail: function sendEmail(template, to) {
     Meteor.Mandrill.sendTemplate({
+      /*eslint-disable */
       template_name: template,
       template_content: [
         {
 
         }
+      /*eslint-enable */
       ],
       message: {
         to: [
