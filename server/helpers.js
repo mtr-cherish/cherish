8adc614640ea4412ce89e1968d5cae4564fbe5b6
Chris Cortese <cortese.consulting@gmail.com>
2015-07-13 19:23:04 -0700

    revert to master this file.  Changes were made as wip towards Login with Facebook which will become a new card.

--- server/helpers.js
+++ server/helpers.js
@@ -1,12 +1,9 @@
 Accounts.onCreateUser(function onCreateUserCallback(options, user) {
-  user.profile = options.profile ? options.profile : {};
-  console.log(user);
-  console.log('usfb:' + user.services.facebook);
-  if (user.services.facebook !== undefined) {
-    options.profile = {avatarImg : 'http://graph.facebook.com/' +
-      user.services.facebook.id + '/picture/?type=large'};
+  if (user.services.facebook) {
+    options.profile.avatarImg = 'http://graph.facebook.com/' + user.services.facebook.id + '/picture/?type=large';
   } else {
-    options.profile = {avatarImg : '/images/placeholder-avatar.jpg'};
+    options.profile.avatarImg = '/images/placeholder-avatar.jpg';
   }
+  user.profile = options.profile ? options.profile : {};
   return user;
 });
